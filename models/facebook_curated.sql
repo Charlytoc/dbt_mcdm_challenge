7ffe4bc263d76f7b4d3729e62f72ba7ff028fd60
Charly Chacón <charlyjchaconc@gmail.com>
2023-04-30 19:06:48 +0000

    Latest changes

--- models/facebook_curated.sql
+++ models/facebook_curated.sql
@@ -1,2 +1,2 @@
 
-SELECT channel, clicks, date, views as conversions, ad_id,spend, impressions  FROM dbt_curated.src_ads_creative_facebook_all_data
+SELECT SUM(purchase) FROM dbt_curated.src_ads_creative_facebook_all_data
